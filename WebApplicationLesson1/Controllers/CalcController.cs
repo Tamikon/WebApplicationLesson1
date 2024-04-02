3e00b667f1c5c3a5428216751cedc06248a5fded
Tamikon <richichichi2207@gmail.com>
2024-04-03 01:10:25 +0300

    Добавил операцию деления

---
+++ WebApplicationLesson1/Controllers/CalcController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b, char c)
+        {
+            if (c == '-') return $"{a} {c} {b} = {a - b}";
+            if (c == '*') return $"{a} {c} {b} = {a * b}";
+            if (c == '/' && b != 0) return $"{a} {c} {b} = {a / b}";
+            if (c == '/' && b == 0) return "Ошибка: делеине на ноль.";
+            if (c != '+' && c != 0) return "Некорректная операция. Применяйте операции умножения, сложения и вычитания.";
+            return $"{a} + {b} = {a + b}";
+        }
+    }
+}
