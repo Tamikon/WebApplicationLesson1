8159899fea475ac34500e7a8eb42aa56d50f9523
Tamikon <richichichi2207@gmail.com>
2024-04-03 00:01:09 +0300

    Улучшил калькулятор: добавил проверку на дурака и дополнительные операции с числами

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, char sign)
+        {
+            if (sign == '-') return $"{a} {sign} {b} = {a - b}";
+            if (sign == '*') return $"{a} {sign} {b} = {a * b}";
+            if (sign != '+' && sign != 0) return "Некорректная операция. Применяйте операции умножения, сложения и вычитания.";
+            return $"{a} + {b} = {a + b}";
+        }
+    }
+}
