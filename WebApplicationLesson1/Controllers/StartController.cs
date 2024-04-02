adfee2204d5f1f4f24688afa183dff867bd00d83
Tamikon <richichichi2207@gmail.com>
2024-04-02 20:59:53 +0300

    задание 2.1.1

---
+++ WebApplicationLesson1/Controllers/StartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            var hour = DateTime.Now.Hour;
+            if (hour < 6)
+            {
+                return "Доброй ночи";
+            }
+            if (hour < 12)
+            {
+                return "Доброе утро";
+            }
+            if (hour < 18)
+            {
+                return "Добрый день";
+            }
+            return "Добрый вечер";
+        }
+    }
+}
